281eca3331bced71f463ae6ee1f4c4e51227fe9a
Derek Haynes <derek.haynes@gmail.com>
2020-04-02 11:14:00 -0600

    Deploying the PyTorch Text Classifier to AWS SageMaker

---
+++ notebooks/03_APIs/pt-text-classification/deploy/sagemaker/deploy.py
@@ -0,0 +1,59 @@
+import sys
+sys.path.append(".")
+import sagemaker
+import os
+import boto3
+import re
+from deploy_env import DeployEnv
+from sagemaker.pytorch import PyTorchModel
+from text_classification import config
+
+env = DeployEnv()
+
+def s3_bucket_from_url(s3_url):
+    return re.search('//(\w+)',s3_url).groups()[0]
+
+def upload_model_data():
+    if env.isLocal():
+        return
+    config.logger.info("Uploading model.tar.gz to S3...")
+    s3 = boto3.resource('s3')
+    return s3.Bucket(s3_bucket_from_url(env.setting('model_data_path'))).upload_file("build/model.tar.gz", "model.tar.gz")
+
+def build_model_data_file():
+    return os.system("tar -czf build/model.tar.gz experiments text_classification logging.json")
+
+def update_endpoint_if_exists():
+    return (env.isProduction() & env.isDeployed())
+
+def delete_endpoint_and_config():
+    """
+    Need to manually delete the endpoint and config because of
+    https://github.com/aws/sagemaker-python-sdk/issues/101#issuecomment-607376320.
+    """
+    env.client().delete_endpoint(EndpointName=env.setting('model_name'))
+    env.client().delete_endpoint_config(EndpointConfigName=env.setting('model_name'))
+
+def deploy():
+    config.logger.info("Deploying model_name=%s to env=%s" % (env.setting('model_name'), env.current_env()))
+    build_model_data_file()
+    upload_model_data()
+    pytorch_model = PyTorchModel(
+        model_data = env.setting('model_data_path'),
+        name = env.setting('model_name'),
+        framework_version = '1.4.0',
+        role = env.setting("aws_role"),
+        env = {"DEPLOY_ENV": env.current_env()},
+        entry_point = 'deploy/sagemaker/serve.py')
+
+    if env.isDeployed():
+        delete_endpoint_and_config()
+
+    predictor = pytorch_model.deploy(
+        instance_type = env.setting('instance_type'),
+        # Below isn't working: https://github.com/aws/sagemaker-python-sdk/issues/101#issuecomment-607376320
+        update_endpoint = update_endpoint_if_exists(),
+        initial_instance_count = 1)
+
+if __name__ == '__main__':
+    deploy()
